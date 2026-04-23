c81c2963c86f3191c252830cb8b838931b6d55c4
pragatiagrawal69 <your.email@example.com>
2026-04-23 20:00:02 +0530

    displays detailed information about a selected book & Include a Back to Browse

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes} from "react-router-dom"
 import NavBar from "./components/NavBar"
 import Home from "./pages/Home.jsx"
 import BrowseBooks from './pages/BrowseBooks'
+import BookDetails from "./pages/BookDetails";
 import { useState } from 'react'
 import './App.css'
 
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="/books/detail/:id" element={<BookDetails />} />
          <Route path="/books" element={<BrowseBooks />} />
          <Route path="/books/:category" element={<BrowseBooks />} />
       </Routes>
---
+++ src/pages/BookDetails.jsx
@@ -0,0 +1,41 @@
+
+import { useParams, Link } from "react-router-dom";
+import { getBookById } from "../data/books";
+
+function BookDetails() {
+  const { id } = useParams(); // URL se id milega
+  const book = getBookById(id);
+
+  if (!book) {
+    return <div className="p-10 text-center">Book not found ❌</div>;
+  }
+
+  return (
+    <div className="min-h-screen bg-slate-100 p-6">
+      <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-md p-6">
+        
+   
+        <div className="flex justify-center mb-6">
+          <img src={book.cover} alt={book.title} className="h-60 object-contain"/>
+        </div>
+
+  
+        <h1 className="text-2xl font-bold mb-2">{book.title}</h1>
+        <p className="text-slate-600 mb-2">by {book.author}</p>
+
+        <p className="text-sm text-blue-500 mb-2">{book.category}</p>
+
+        <p className="mb-4 text-slate-700">{book.description}</p>
+
+        <p className="text-sm mb-1">📄 Pages: {book.pages}</p>
+        <p className="text-sm mb-1">📅 Year: {book.year}</p>
+
+        <div className="mt-6">
+          <Link to="/books" className="bg-slate-900 text-white px-4 py-2 rounded-lg"> ← Back to Books</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookDetails;
