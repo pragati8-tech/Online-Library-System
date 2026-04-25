dbd64db307d61810d7f0778b7dd1d7ae80e5a368
pragatiagrawal69 <your.email@example.com>
2026-04-25 16:31:20 +0530

    delete node_module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+// 🔹 Export Vite configuration
 export default defineConfig({
   plugins: [
     react(),
